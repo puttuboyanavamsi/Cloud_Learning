de76c47dd757b047b5808c47387ffdb6f3cea877
puttuboyanavamsi <122369406+puttuboyanavamsi@users.noreply.github.com>
2025-10-31 17:10:35 +0530

    Added comments

--- orchestartor/Terafform_Learning/Main.tf
+++ orchestartor/Terafform_Learning/Main.tf
@@ -1,4 +1,7 @@
 #Terfform install packages by running this command (terfform init)
+# To see the plan (terfform plan)
+# To apply (terfform apply)
+# 
 
 provider "aws" {
   region = "ap-southeast-2"
