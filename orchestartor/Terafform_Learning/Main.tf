e7c22c090b85b919dd73e7b4ce2d46a12d5dc90e
puttuboyanavamsi <122369406+puttuboyanavamsi@users.noreply.github.com>
2025-11-03 15:29:51 +0530

    added document file

--- orchestartor/Terafform_Learning/Main.tf
+++ orchestartor/Terafform_Learning/Main.tf
@@ -7,7 +7,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "Testing1" {
+resource "aws_instance" "Testing" {
   ami           = "ami-0279a86684f669718"
   instance_type = "t3.micro"
 
