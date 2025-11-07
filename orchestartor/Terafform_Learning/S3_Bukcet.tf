5f5edaadb99e92630003906140a4e75390f3ea7d
puttuboyanavamsi <122369406+puttuboyanavamsi@users.noreply.github.com>
2025-11-07 15:20:55 +0530

    Update S3_Bukcet.tf

--- orchestartor/Terafform_Learning/S3_Bukcet.tf
+++ orchestartor/Terafform_Learning/S3_Bukcet.tf
@@ -8,3 +8,12 @@ resource "aws_s3_bucket" "my_s3_bucket1" {
     Environment = "Stg"
   }
 }
+
+# versioning resource
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_s3_bucket1.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
